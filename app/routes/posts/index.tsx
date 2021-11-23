55784cc47b9697f1ca02f6aeab2d2912957f1939
suzie <suuuzie18@gmail.com>
2021-11-24 03:22:15 +0900

    useLoaderData를 사용하면 컴포넌트가 API가 된다?

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,4 +1,21 @@
+import {useLoaderData} from 'remix'
+
+export let loader = () => {
+  return [
+    {
+      slug: 'my-first-post',
+      title: 'My First Post',
+    },
+    {
+      slug: '90s-mixtape',
+      title: 'A Mixtape I Made Just For You',
+    },
+  ]
+}
+
 export default function Posts() {
+  let posts = useLoaderData()
+  console.log(posts)
   return (
     <div>
       <h1>Posts</h1>
